965582c576888912c9fbef0116ece52d2e5d4b76
shibayu36 <shibayu36@gmail.com>
2023-05-10 16:52:00 +0900

    Remove useless

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { Hono } from "hono";
 
 const app = new Hono();
