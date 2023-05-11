f7fcd16c2c9d2f5c6f499157ae8b18d476622f90
shibayu36 <shibayu36@gmail.com>
2023-05-11 10:47:59 +0900

    Also publish secret when deploying

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+wrangler publish
+
+# Parse .dev.vars and set secrets
+if [[ ! -f .dev.vars ]]; then
+    echo "File .dev.vars not found!"
+    exit 1
+fi
+while IFS='=' read -r name value
+do
+  echo "Setting $name"
+  echo $value | wrangler secret put $name
+done < .dev.vars
