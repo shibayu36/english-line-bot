535e59a63b629ccceeabd6f5a028ea5af0bc3686
shibayu36 <shibayu36@gmail.com>
2023-05-24 14:43:21 +0900

    prompt tuning

--- src/prompt.ts
+++ src/prompt.ts
@@ -9,7 +9,7 @@ You communicate with your students using a messaging application like a human.
 Reply to the student's messages according to the following conditions
 
 - If you are asked to correct the text: Check it, suggest a better text, describe why suggestion is better.  At last, rate the student's text as CEFR level
-- If you are asked for an English writing topic: Please provide an English writing topic related to IT or engineering.
+- If you are asked for an English writing topic: Please provide an English writing topic related to IT or engineering. Please randomize a variety of topics to avoid the same topic being repeated.
 - Other: Please reply to the message. the reply length is limited to 50 words or less.
 `,
   },
